a2666d5d518458d1da9d8fb8a69a71a70d39889d
Paulo Vitor Carvalho Santos <p.vitor_8@hotmail.com>
2018-04-01 23:42:39 -0300

    Add files via upload

---
+++ js/funcoes.js
@@ -0,0 +1,16 @@
+
+		//alerta javasrcript puro
+		function alerta() {
+		    alert("DESCULPA, A página que você esta tentando acessar esta em construção!");
+		}
+
+		//Script de alerta usando plugin swwetalert
+		function funcao() {
+		    swal("Obrigado!", "Você curtiu este livro!", "success");
+		}
+		
+		//Script para inicializar animação do plugin wow js-->
+				$(function() {
+				new WOW().init();
+				});
+			
